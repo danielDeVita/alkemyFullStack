a897e0f614e9e7443f8549d03c79d3be126e9519
Dani <danielitodevita@gmail.com>
2022-08-16 15:55:19 -0300

    pushed react app stuff

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const methodOverride = require("method-override");
+const cors = require("cors");
 
 const routes = require("./routes");
 
@@ -12,6 +13,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.use(cors());
 
 app.use('/', routes)
 
