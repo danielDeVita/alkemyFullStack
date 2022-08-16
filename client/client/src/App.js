b13497492fa85c8448744599a60a6d0915bf5f5a
Dani <danielitodevita@gmail.com>
2022-08-16 15:48:58 -0300

    fetching data from API at client react app OK

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
+/* import './App.css'; */
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [list, setList] = useState([]);
+
+  const fetchData = async () => {
+    const data = await fetch("http://localhost:3001/api/")
+    const entries = await data.json()
+    setList(entries)
+  }
+
+  useEffect(
+    () => {
+      fetchData()
+    }, []
+  )
+
   return (
     <div className="App">
+      <ul>
+      {
+        list.map(item =>(
+          <li key={item.id}>Concepto: {item.concepto} - Monto: ${item.monto} - Fecha: {item.fecha} - Tipo: {item.tipo}</li>
+        ))
+      }
+      </ul>
     </div>
   );
 }
